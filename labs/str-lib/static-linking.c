41263ba1508ce8533dfea8f5843b0eec080f6515
Jose Adolfo <jmicalco@gmail.com>
2021-03-23 06:21:25 +0000

    Solve str-lib
    Signed-off-by: Jose Adolfo <jmicalco@gmail.com>

--- labs/str-lib/static-linking.c
+++ labs/str-lib/static-linking.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc<4)
+    {
+        printf("You are missing the second string to apply the %s functionality\n",argv[1]);  
+    } else {
+        if (strcmp(argv[1],"-add")==0)
+        {
+            mystradd(argv[2],argv[3]);
+        } else if (strcmp(argv[1],"-find")==0)
+        {
+            if (mystrfind(argv[2],argv[3])!=0)
+            {
+                printf("['%s'] string was found at [%i] position\n", argv[3], mystrfind(argv[2],argv[3]));
+            }
+               
+        }
+        
+        
+    }
+    
+
     return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,75 @@
 
 
 int mystrlen(char *str){
-    return 0;
+    int i=0;
+    while (*str)
+    {
+        str++;
+        i++;
+    }
+    return i;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    char newString[mystrlen(origin)+mystrlen(addition)];
+    int i=0;
+    int size=mystrlen(origin)+mystrlen(addition);
+    printf("Initial Length  : %i\n",mystrlen(origin));
+    printf("New String      : ");
+    while (i<size)
+    {
+        if (*origin)
+        {
+            newString[i]=*origin;
+            printf("%c",newString[i]);
+            origin++;
+            i++;
+        } else {
+            break;
+        }
+    }
+    while (i<size)
+    {
+        if (*addition)
+        {
+            newString[i]=*addition;
+            printf("%c",newString[i]);
+            addition++;
+            i++;
+        }
+        else {
+            break;
+        }
+    }
+    printf("\n");
+    printf("New Length      : %i\n",size);
 }
 
 int mystrfind(char *origin, char *substr){
+    int sizeSub=mystrlen(substr);
+    char *beginning= substr;
+    int i=0, k=0;
+    while(*origin){
+        if(*origin==*substr){
+            while(*substr){
+                if(*substr==*origin){
+                    i++;
+                    substr++;
+                    origin++;
+                } else {
+                    break;
+                }
+                if(i==sizeSub){
+                    return k;
+                }
+            }
+            k++;
+            i=0;
+            substr=beginning;
+        } else {
+            k++;
+            origin++;
+        }
+    }
     return 0;
 }
