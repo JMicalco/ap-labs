b2437fc9ea2942edc60c27ba167aa5d1402f17d5
Jose Adolfo <jmicalco@gmail.com>
2021-04-13 23:44:16 +0000

    Solve Logger
    Signed-off-by: Jose Adolfo <jmicalco@gmail.com>

--- labs/logger/testLogger.c
+++ labs/logger/testLogger.c
@@ -1,5 +1,15 @@
 
+int infof(const char *format, ...);
+int warnf(const char *format, ...);
+int errorf(const char *format, ...);
+int panicf(const char *format, ...);
 
 int main() {
+    
+    infof("INFO");
+    warnf("WARNING");
+    errorf("ERROR");
+    panicf("PANIC");
+
     return 0;
 }
